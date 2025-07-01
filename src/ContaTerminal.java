b77ded3c77b8ebaa00543171a00d68b221ac95f6
Thaís Alcantara <tatie.alcantara@gmail.com>
2025-06-30 21:11:41 -0300

    basic project final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner accountInfo = new Scanner(System.in);
-        System.out.print("Bem vindo ao seu Banco Digital! \n Por favor, digite o número da sua conta: \n");
+        System.out.print("Bem vindo ao seu Banco Digital! \nPor favor, digite o número da sua conta: \n");
         var accountNumber = accountInfo.nextInt();
+        accountInfo.nextLine();
         System.out.println("Agora digite o número da sua Agência: ");
-        var accountAgency = accountInfo.next();
+        var accountAgency = accountInfo.nextLine();
         System.out.println("Digite seu nome completo: ");
-        var accoountOwner = accountInfo.next();
-        System.out.println();
+        var accountOwner = accountInfo.nextLine();
+        System.out.println("Digite o saldo desejado para saque: ");
+        var withdrawValue = accountInfo.nextFloat();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", accountOwner, accountAgency, accountNumber, withdrawValue);
+        accountInfo.close();
     }
 
 }
